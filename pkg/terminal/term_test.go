882753f7d4367cde3844e2c489848627246beb48
Alessandro Corradi <corradi.alessandro@mmfg.it>
2025-06-30 13:35:19 +0200

    feat: add test implementation

--- pkg/terminal/term_test.go
+++ pkg/terminal/term_test.go
@@ -1,10 +1,26 @@
 package terminal_test
 
 import (
-    "testing"
-    "codeberg.org/ale-cci/connect/pkg/terminal"
+	"bufio"
+	"bytes"
+	"testing"
+
+	"codeberg.org/ale-cci/connect/pkg/terminal"
 )
 
 
 func TestTerminal(t *testing.T) {
+    input := bytes.Buffer{}
+    output := bytes.Buffer{}
+
+    term := terminal.Terminal{
+        Input: *bufio.NewReader(&input),
+        Output: &output,
+        Prompt: ">",
+    }
+
+    _, err := term.ReadCmd()
+    if err == nil {
+        t.Errorf("expected EOF, got no errors")
+    }
 }
