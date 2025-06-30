4e5a074930fda4e3a760a6296301a03fdc78558f
Alessandro Corradi <corradi.alessandro@mmfg.it>
2025-06-30 13:20:55 +0200

    feat: multiplatform config

--- pkg/terminal/term.go
+++ pkg/terminal/term.go
@@ -37,7 +37,7 @@ const (
 )
 
 func (t *Terminal) ReadCmd() (cmd string, err error) {
-	command := [][]rune{[]rune{}}
+	command := [][]rune{{}}
 	t.Output.Write([]byte(t.Prompt))
 
 	escapeRune := '\x00'
@@ -203,8 +203,9 @@ func isPrintable(r rune) bool {
 type State struct {
 	termios unix.Termios
 }
+
 func MakeRaw(fd int) (*State, error) {
-	termios, err := unix.IoctlGetTermios(fd, unix.TCGETS)
+	termios, err := unix.IoctlGetTermios(fd, ioctlReadTermios)
 	if err != nil {
 		return nil, err
 	}
@@ -219,7 +220,7 @@ func MakeRaw(fd int) (*State, error) {
 	termios.Cflag |= unix.CS8
 	termios.Cc[unix.VMIN] = 1
 	termios.Cc[unix.VTIME] = 0
-	if err := unix.IoctlSetTermios(fd, unix.TCSETS, termios); err != nil {
+	if err := unix.IoctlSetTermios(fd, ioctlWriteTermios, termios); err != nil {
 		return nil, err
 	}
 
@@ -227,5 +228,5 @@ func MakeRaw(fd int) (*State, error) {
 }
 
 func Restore(fd int, state *State) error {
-	return unix.IoctlSetTermios(fd, unix.TCSETS, &state.termios)
+	return unix.IoctlSetTermios(fd, ioctlWriteTermios, &state.termios)
 }
---
+++ pkg/terminal/term_macos.go
@@ -0,0 +1,7 @@
+//go:build darwin || dragonfly || freebsd || netbsd || openbsd
+package terminal
+
+import "golang.org/x/sys/unix"
+
+const ioctlReadTermios = unix.TIOCGETA
+const ioctlWriteTermios = unix.TIOCSETA
